3262290ffc7524c48094a3642b55bae23c7d5996
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-03 11:55:15 -0400

    added json server and inital fetches to the back end and stored them in state

--- fitnessgram-phase2-project/src/components/App.js
+++ fitnessgram-phase2-project/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import ExerciseList from "./ExerciseList";
 import BlogList from "./BlogList";
@@ -6,6 +6,19 @@ import NewBlogForm from "./NewBlogForm";
 import Header from "./Header";
 
 function App() {
+  const [exercises, setExercises] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/exercises`)
+      .then((res) => res.json())
+      .then((data) => setExercises(data));
+
+    fetch(`http://localhost:3000/blogs`)
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
   return (
     <div className="App">
       <Header />
