3aec37bc8c8f1fc04400b944af77b9ec8b62ae9c
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-03 12:43:16 -0400

    exercise list renders card and user can search by name or body part

--- fitnessgram-phase2-project/src/components/ExerciseCard.js
+++ fitnessgram-phase2-project/src/components/ExerciseCard.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function ExerciseCard() {
-  return <div>ExerciseCard</div>;
+function ExerciseCard({ name, bodyPart, weight, reps, sets }) {
+  return (
+    <div className="exercise-card">
+      <h2>{name}</h2>
+      <h3>Body Part: {bodyPart}</h3>
+      <h4>Weight: {weight}</h4>
+      <h4>Reps: {reps}</h4>
+      <h4>Sets: {sets}</h4>
+    </div>
+  );
 }
 
 export default ExerciseCard;
--- fitnessgram-phase2-project/src/components/ExerciseList.js
+++ fitnessgram-phase2-project/src/components/ExerciseList.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ExerciseCard from "./ExerciseCard";
 
-function ExerciseList() {
-  return <div>ExerciseList</div>;
+function ExerciseList({ exercises }) {
+  const exerciseCards = exercises.map((exercise) => {
+    return <ExerciseCard key={exercise.id} {...exercise} />;
+  });
+  return <div className="exercise-list">{exerciseCards}</div>;
 }
 
 export default ExerciseList;
